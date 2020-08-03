c652ff8a256271595bd5afd3e79c963cc3ab015e
BirdOnTheBranch <planelles.luis@gmail.com>
2020-08-03 17:28:44 +0200

    Created the new app shop with its urls, views(list and detail), models(Product, Categories) and admin.py

--- store/settings.py
+++ store/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'ap$@(p=7*ayfk)-omy_-$y%hd%33^nc9a3)xovy)u4b8d$k)9='
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'shop.apps.ShopConfig',
 ]
 
 MIDDLEWARE = [
