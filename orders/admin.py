14c753ac722255c66dc41cdd9fcbf752bdd6dc2c
BirdOnTheBranch <planelles.luis@gmail.com>
2020-08-18 11:35:47 +0200

    Edited admin in orders  to download files csv from orders list.

--- orders/admin.py
+++ orders/admin.py
@@ -1,5 +1,32 @@
 from django.contrib import admin
+from django.http import HttpResponse
+import csv
+import datetime
+
 from .models import Order, OrderItem
+
+
+
+def export_to_csv(modeladmin, request, queryset):
+    opts = modeladmin.model._meta 
+    response = HttpResponse(content_type='text/csv')
+    response['content-disposition'] = 'attachment;''filename{}.csv'.format(opts.verbose_name)
+    writer = csv.writer(response)
+    fields = [field for field in opts.get_fields() if not field.many_to_many and not field.one_to_many ]
+    #include the first file with header
+    writer.writerow([field.verbose_name for field in fields])
+    #include data rows
+    for obj in queryset:
+        data_row = []
+        for field in fields:
+            value = getattr(obj, field.name) 
+            if isinstance(value, datetime.datetime):
+                value = value.strftime('%d/%m/%Y')
+            data_row.append(value)
+        writer.writerow(data_row)
+    return response 
+
+export_to_csv.short_description = 'Export to CSV'
 
 
 
@@ -15,3 +42,4 @@ class OrderAdmin(admin.ModelAdmin):
                     'created', 'updated']
     list_filter = ['paid', 'created', 'updated']
     inlines = [OrderItemInline]
+    actions = [export_to_csv]
